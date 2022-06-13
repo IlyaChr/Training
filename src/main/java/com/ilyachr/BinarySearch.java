70b0859dfbbea5e02be947cee02beaf174e68153
DESKTOP-3P7LL56\CherkashchenkoIA <ilyachr@yahoo.com>
2022-06-13 13:30:13 +0600

    Leet code - init 704. Binary Search

---
+++ src/main/java/com/ilyachr/BinarySearch.java
@@ -0,0 +1,31 @@
+package com.ilyachr;
+
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+
+        return nums.length == 0 ? -1 : findInMidle(nums, 0, nums.length, target);
+    }
+
+    private int findInMidle(int[] nums, int a, int b, int target) {
+
+        if (b - a == 0 || b - a == 1) {
+
+            if (target == nums[a]) {
+                return a;
+            }
+
+            return -1;
+        }
+
+
+        int result = nums[b - (b - a) / 2];
+
+        if (result == target) {
+            return b - (b - a) / 2;
+        }
+
+        return target < result ? findInMidle(nums, a, b - (b - a) / 2, target) : findInMidle(nums, b - (b - a) / 2, b, target);
+    }
+
+}
---
+++ src/test/java/com/ilyachr/BinarySearchTest.java
@@ -0,0 +1,30 @@
+package com.ilyachr;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BinarySearchTest {
+
+    @Test
+    void search() {
+
+        BinarySearch binarySearch = new BinarySearch();
+
+        assertEquals(4, binarySearch.search(Stream.of(-1, 0, 3, 5, 9, 12).mapToInt(i -> i).toArray(), 9));
+
+        assertEquals(-1, binarySearch.search(Stream.of(-1, 0, 3, 5, 6, 12).mapToInt(i -> i).toArray(), 9));
+
+        assertEquals(2, binarySearch.search(IntStream.of(-1, 0, 3, 5, 9, 12).toArray(), 3));
+
+        assertEquals(-1, binarySearch.search(IntStream.of().toArray(), 3));
+
+        assertEquals(0, binarySearch.search(IntStream.of(3).toArray(), 3));
+
+        assertEquals(-1, binarySearch.search(IntStream.of(3).toArray(), 4));
+
+    }
+}
